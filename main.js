b7d206f37adb453c49e71d7410af7cf05e07c51d
akshayk2001 <email.akshay.k@gmail.com>
2016-05-21 11:06:18 -0700

    Refactoring Code. Javascript moved to separate file.

---
+++ main.js
@@ -0,0 +1,37 @@
+var width = 500;
+var height = 500;
+var canvas = document.getElementById('asteroids');
+canvas.width = width;
+canvas.height = height;
+var rectWidth = width/5;
+var rectHeight = height/5;
+var context = canvas.getContext('2d');
+
+
+var x = 0;
+var y = 0 - rectHeight;
+
+var movementRate = 1;
+
+function draw(){
+  //call clearRect with the entire size of the canvas to clear
+  // the whole thing
+
+  context.fillStyle = "rgb(0, 0, 0)";
+  context.fillRect(0, 0, width, height);
+
+  // Drawing triangle with paths
+  context.strokeStyle = 'blue';
+  context.beginPath();
+  context.moveTo(x, y);
+  context.lineTo(x + rectWidth, y);
+  context.lineTo(x + rectWidth/2, y + rectHeight);
+  context.closePath();
+  context.stroke();
+
+  //Update x and y for the next draw operation
+  y = y + movementRate;
+
+  window.requestAnimationFrame(draw);
+}
+window.requestAnimationFrame(draw);
