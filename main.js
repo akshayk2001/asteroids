99c489d4e5ebf140b9470a5c4a48a043ad2c71d6
akshayk2001 <email.akshay.k@gmail.com>
2016-05-28 10:14:43 -0700

    Drawing perimeters of context after each context operation

--- main.js
+++ main.js
@@ -54,41 +54,92 @@ var context = canvas.getContext('2d');
     // context.stroke();
 
 
+var x = 0;
+var y = 0;
+var rotationDegrees = 0;
 
+var movementRate = .1;
+var rotationRate = .1;
 
-    var x = 0;
-    var y = 0;
+function draw(){
+	// call clearRect with the entire size of the canvas to clear the whole thing
+	// Fill the whole canvas with a black rectangle
+	context.fillStyle = "rgb(0, 0, 0)";
+	context.fillRect(0, 0, width, height);
 
-    var movementRate = 1;
+	drawTriangle(context, x, y, rectWidth, rectHeight, rotationDegrees);
 
-    function draw(){
-      //call clearRect with the entire size of the canvas to clear
-      // the whole thing
+	// Update x and y for the next draw operation
+	y = y + movementRate;
+	// x = x + movementRate;
+	rotationDegrees = rotationDegrees + rotationRate;
 
-      context.fillStyle = "rgb(0, 0, 0)";
-      context.fillRect(0, 0, width, height);
+	if (rotationDegrees > 360){
+		rotationDegrees = 0;
+	}
 
-      // Drawing triangle with paths
-      drawTriangle(context, x, y, rectWidth, rectHeight);
-      //Update x and y for the next draw operation
-
-
-      window.requestAnimationFrame(draw);
-    }
-    window.requestAnimationFrame(draw);
-
-
-
-    function drawTriangle(context, x, y, rectWidth, rectHeight){
-      // Drawing triangle with paths
-      context.strokeStyle = 'blue';
-      context.beginPath();
-      context.moveTo(x, y);
-      context.lineTo(x + rectWidth, y + rectHeight/2);
-      context.lineTo(x, y + rectHeight);
-      context.closePath();
-      context.stroke();
-      context.translate(width/2 - rectWidth/2, height/2 - rectHeight/2);
-      context.rotate(45);
-
+	window.requestAnimationFrame(draw);
 }
+window.requestAnimationFrame(draw);
+
+
+
+function drawTriangle(context, x, y, rectWidth, rectHeight, rotationDegrees){
+	context.save();
+
+	var centerX = x + rectWidth/2;
+	var centerY = y + rectHeight/2;
+
+	context.translate(centerX, centerY); // Translate to center of rectangle
+	console.log('context.translate(centerX, centerY)', centerX, centerY);
+
+	context.strokeStyle = "white";
+	context.strokeRect(0, 0, width, height);
+
+	// Convert degrees to radians, because context.rotate needs radians
+	var radians = rotationDegrees * (Math.PI/180);
+	context.rotate(radians);
+	console.log('rotationDegrees', rotationDegrees);
+	console.log('context.rotate(radians)', radians);
+
+	context.strokeStyle = "yellow";
+	context.strokeRect(0, 0, width, height);
+
+	context.translate(-centerX, -centerY);
+	console.log('context.translate(-centerX, -centerY);', -centerX, -centerY);
+
+	context.strokeStyle = "green";
+	context.strokeRect(0, 0, width, height);
+
+
+
+	// Draw a triangle with paths
+	context.strokeStyle = 'red';
+	// Start a new path
+	context.beginPath();
+
+	// Now define the path with .moveTo() and .lineTo() methods
+	context.moveTo(x, y);
+	context.lineTo(x + rectWidth, y);
+	context.lineTo(x + rectWidth/2, y + rectHeight);
+
+	context.closePath();
+
+	// The path is only made visible when we call .stroke()
+	context.stroke();
+
+	context.restore();
+}
+
+
+//     function drawTriangle(context, x, y, rectWidth, rectHeight){
+//       // Drawing triangle with paths
+//       context.strokeStyle = 'blue';
+//       context.beginPath();
+//       context.moveTo(x, y);
+//       context.lineTo(x + rectWidth, y + rectHeight/2);
+//       context.lineTo(x, y + rectHeight);
+//       context.closePath();
+//       context.stroke();
+//
+// }
